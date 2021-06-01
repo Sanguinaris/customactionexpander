1c96fff1accd76720900488a41a898b47b9f2dac
Sanguinaris <wieichdashasse@gmail.com>
2021-06-01 17:53:42 +0100

    added check and small cleanup

--- main.go
+++ main.go
@@ -14,8 +14,8 @@ func check(e error) {
 }
 
 func main() {
-	if len(os.Args) < 3 {
-		fmt.Printf("Usage: %s SOURCEFILE OUTPUTDIR\n", os.Args[0])
+	if len(os.Args) < 2 {
+		fmt.Printf("Usage: %s SOURCEFILE\n", os.Args[0])
 		return
 	}
 
@@ -23,18 +23,34 @@ func main() {
 	check(err)
 
 	overlaySection := f.Sections[f.FileHeader.NumberOfSections-1].SectionHeader.Size + f.Sections[f.FileHeader.NumberOfSections-1].SectionHeader.Offset
-	fmt.Println(overlaySection)
+	fmt.Println(os.Args[1], overlaySection)
 
 	read, err := os.Open(os.Args[1])
 	check(err)
 	write, err := os.Create(os.Args[1] + ".cab")
 	check(err)
 
-	read.Seek(int64(overlaySection), 0)
+	_, err = read.Seek(int64(overlaySection), 0)
+	check(err)
 
 	p := make([]byte, 4)
+
+	// check that it's actually a cab and we're not at the end
+	n, err := read.Read(p)
+	if err == io.EOF {
+		fmt.Println("No Overlay found!")
+		return
+	}
+	if string(p[:n]) != "MSCF" {
+		fmt.Println("Don't think this overlay data is a cab")
+		return
+	}
+	fmt.Println("found cab!")
+	write.Write(p[:n])
+
+	// do the rest
 	for {
-		n, err := read.Read(p)
+		n, err = read.Read(p)
 		if err == io.EOF {
 			break
 		}
